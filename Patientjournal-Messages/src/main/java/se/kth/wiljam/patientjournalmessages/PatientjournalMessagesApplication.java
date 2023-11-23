79d98f923a045515d535d6fe8380bf34567558b1
NillRudd <102033730+NillRudd@users.noreply.github.com>
2023-11-23 10:33:14 +0100

    Add files via upload
    init messages microservice

---
+++ Patientjournal-Messages/src/main/java/se/kth/wiljam/patientjournalmessages/PatientjournalMessagesApplication.java
@@ -0,0 +1,13 @@
+package se.kth.wiljam.patientjournalmessages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientjournalMessagesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientjournalMessagesApplication.class, args);
+    }
+
+}
---
+++ Patientjournal-Messages/src/test/java/se/kth/wiljam/patientjournalmessages/PatientjournalMessagesApplicationTests.java
@@ -0,0 +1,13 @@
+package se.kth.wiljam.patientjournalmessages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientjournalMessagesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
